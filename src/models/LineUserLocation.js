ad5fd5db054f3b9b3d8e9833ba67cde49e3737fd
funtuan <hank.chen@ohbottech.com>
2022-04-29 14:36:06 +0800

    feat LineUserLocation index

--- src/models/LineUserLocation.js
+++ src/models/LineUserLocation.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const LineUserLocation = new Schema({
-  userId: { type: String, required: true },
+  userId: { type: String, required: true, index: true },
   address: { type: String, required: true },
   lat: { type: Number, required: true },
   lng: { type: Number, required: true },
