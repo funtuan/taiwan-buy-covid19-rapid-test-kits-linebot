ae54b07f54af226faadbc71a51c554454f03a4c3
funtuan <hank.chen@ohbottech.com>
2022-04-29 16:08:24 +0800

    Fix LineUserLocation log

--- src/models/LineUserLocation.js
+++ src/models/LineUserLocation.js
@@ -14,7 +14,7 @@ const Model = mongoose.model('LineUserLocation', LineUserLocation)
 Model.findByUserId = async (userId) => {
   const lineUserLocation = await Model.findOne({ userId })
 
-  console.log('[LineUserLocation]', lineUserLocation ? lineUserLocation.toJSON() : 'not save')
+  console.log('[LineUserLocation]', lineUserLocation ? lineUserLocation.address : 'not save')
   if (!lineUserLocation) {
     return {
       address: '台北市中正區林森南路6號',
