4e033b748d2eb9fb14b4cee8b5bde4a42e90d88f
funtuan <hank.chen@ohbottech.com>
2022-04-29 09:00:41 +0800

    Add env CRAWLER

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 
+require('dotenv').config()
+
 require('./db')()
 require('./services/lineBot')()
-require('./services/pointData')()
+if (process.env.CRAWLER !== 'false') {
+  require('./services/pointData')()
+}
 require('./services/pointEngine').init()
