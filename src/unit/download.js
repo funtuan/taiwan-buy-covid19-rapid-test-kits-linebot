36c2c22cc53ccd3cc99eaa20b3602874b9cdbae8
funtuan <hank.chen@ohbottech.com>
2022-04-29 10:55:05 +0800

    優化 downloadCSV

--- src/unit/download.js
+++ src/unit/download.js
@@ -1,11 +1,8 @@
 
-const request = require('request')
+const axios = require('axios')
 const csv = require('csvtojson')
-const iconv = require('iconv-lite')
 
-module.exports.downloadCSV = (url, {
-  format = 'utf8',
-} = {}) => {
-  return csv()
-      .fromStream(request.get(url).pipe(iconv.decodeStream(format)))
+module.exports.downloadCSV = async (url) => {
+  const csvString = await axios.get(url)
+  return await csv().fromString(csvString.data)
 }
